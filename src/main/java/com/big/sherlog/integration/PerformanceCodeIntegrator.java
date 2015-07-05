ba64b802e4cb508491c711f5aa87b519e1d15a1c
pk039 <pk039@hdm-stuttgart.de>
2015-07-05 21:33:57 +0200

    removed debug statements from PerformanceCodeIntegrator

--- src/main/java/com/big/sherlog/integration/PerformanceCodeIntegrator.java
+++ src/main/java/com/big/sherlog/integration/PerformanceCodeIntegrator.java
@@ -7,47 +7,50 @@ import javassist.CtMethod;
 /**
  * Provided implementation of {@link BaseCodeIntegrator}, inserting code for
  * performance measuring of method executions.<br/>
- *
+ * <p>
  * Created by patrick.kleindienst on 03.06.2015.
  *
  * @author patrick.kleindienst
  */
 public class PerformanceCodeIntegrator extends BaseCodeIntegrator {
 
-	// #####################################################
-	// # STATIC MEMBERS #
-	// #####################################################
+    // #####################################################
+    // # STATIC MEMBERS #
+    // #####################################################
 
-	private static final String	SET_START		= "startTime = System.currentTimeMillis();";
-	private static final String	SET_STOP		= "stopTime = System.currentTimeMillis();";
-	private static final String	LOG_START		= PROVIDED_LOGGER + ".debug(\"Execution started at: \" + java.lang.String.valueOf(startTime));";
-	private static final String	LOG_STOP		= PROVIDED_LOGGER + ".debug(\"Execution stopped at: \" + java.lang.String.valueOf(stopTime));";
-	private static final String	LOG_TIME_DIFF	= PROVIDED_LOGGER + ".debug(\"Execution took \" + java.lang.String.valueOf(stopTime - startTime) + \" ms\");";
+    private static final String SET_START = "startTime = System.currentTimeMillis();";
+    private static final String SET_STOP = "stopTime = System.currentTimeMillis();";
+    private static final String LOG_START = PROVIDED_LOGGER + ".debug(\"Execution started at: \" + java.lang.String" +
+            ".valueOf(startTime));";
+    private static final String LOG_STOP = PROVIDED_LOGGER + ".debug(\"Execution stopped at: \" + java.lang.String" +
+			".valueOf(stopTime));";
+    private static final String LOG_TIME_DIFF = PROVIDED_LOGGER + ".debug(\"Execution took \" + java.lang.String" +
+			".valueOf(stopTime - startTime) + \" ms\");";
 
-	// #####################################################
-	// # INSTANCE METHODS #
-	// #####################################################
+    // #####################################################
+    // # INSTANCE METHODS #
+    // #####################################################
 
-	/**
-	 * Adding some code to create performance measurement log statements
-	 * 
-	 * @param ctMethod
-	 *            method to be modified
-	 * @return the modified {@link CtMethod} which can be re-attached to it's
-	 *         declaring class after modification
-	 */
-	@Override
-	protected CtMethod enhanceMethodCode(CtMethod ctMethod) {
-		try {
-			ctMethod.addLocalVariable("startTime", CtClass.longType);
-			ctMethod.addLocalVariable("stopTime", CtClass.longType);
-			ctMethod.insertBefore(SET_START + LOG_START);
-			ctMethod.insertAfter(SET_STOP);
-			ctMethod.insertAfter(LOG_STOP);
-			ctMethod.insertAfter(LOG_TIME_DIFF);
-		} catch (CannotCompileException e) {
-			e.printStackTrace();
-		}
-		return ctMethod;
-	}
+    /**
+     * Adding some code to create performance measurement log statements
+     *
+     * @param ctMethod method to be modified
+     * @return the modified {@link CtMethod} which can be re-attached to it's
+     * declaring class after modification
+     */
+    @Override
+    protected CtMethod enhanceMethodCode(CtMethod ctMethod) {
+        try {
+            ctMethod.addLocalVariable("startTime", CtClass.longType);
+            ctMethod.addLocalVariable("stopTime", CtClass.longType);
+            ctMethod.insertBefore(SET_START + LOG_START);
+            ctMethod.insertAfter(SET_STOP);
+            ctMethod.insertAfter(LOG_STOP);
+            ctMethod.insertAfter(LOG_TIME_DIFF);
+
+        } catch (CannotCompileException e) {
+            e.printStackTrace();
+        }
+        return ctMethod;
+    }
 }
